304febd0b026b934cef3a5928c9c5f56b8f29a78
Yasar <yasar.mohideen@nkw.pub>
2025-12-30 20:23:08 +0530

    fix: remove trailing slash from base path and adjust script source in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ck4-mathlive.github.io/',
+  base: '/ck4-mathlive.github.io',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
